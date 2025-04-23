4b14c5687675da816c64414e2c0720e91f95e825
umar-uzdenov <90197962+umar-uzdenov@users.noreply.github.com>
2025-04-23 18:48:05 +0300

    Commit all current changes

--- backend/server.js
+++ backend/server.js
@@ -80,13 +80,13 @@ app.post('/api/auth', async (req, res) => {
   // Extract user data
   const user = JSON.parse(params.get('user'));
 
-  console.log(await database.user.get(params.get("id")))
-  if (database.user.get(user.id).error) {
+//   console.log(await database.user.get(params.get("id")))
+//   if (database.user.get(user.id).error) {
     database.user.add({
         tgid: user.id, hash, username: user.username, lastRequest: Date.now(),
         balance: 0, models: [], yarns: [], machines: [], items: []
     })
-  }
+//   }
   
   // Implement: Create user in DB if new, then start session
   // Example: Use cookies or JWT
