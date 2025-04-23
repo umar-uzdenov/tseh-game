54ef32bac879f6591eb40cba9f2f6420b99ecc0c
umar-uzdenov <90197962+umar-uzdenov@users.noreply.github.com>
2025-04-23 16:14:42 +0300

    Ensure main.js is up to date

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ const path = require('path');
 
 
 // API routes
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
-});
+// app.get('/api/test', (req, res) => {
+//   res.json({ message: 'Hello from Express!' });
+// });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
 
 // Serve static files in production
 // if (process.env.NODE_ENV === 'production') {
@@ -36,19 +36,19 @@ app.listen(port, () => {
 
 // Serve static files (e.g., HTML, CSS, JS)
 // app.use(express.static(path.join(__dirname, 'static')));
-// app.use(express.static(path.join(__dirname, 'tg-test')));
+app.use(express.static(path.join(__dirname, 'tg-test')));
 
 
 // SSL Certificate (Replace paths with your certificate files)
-// const sslOptions = {
-//   key: fs.readFileSync('../../etc/letsencrypt/live/tseh-game.ru/privkey.pem'),
-//   cert: fs.readFileSync('../../etc/letsencrypt/live/tseh-game.ru/fullchain.pem')
-// };
+const sslOptions = {
+  key: fs.readFileSync('../../etc/letsencrypt/live/tseh-game.ru/privkey.pem'),
+  cert: fs.readFileSync('../../etc/letsencrypt/live/tseh-game.ru/fullchain.pem')
+};
 
 // HTTPS Server
-// https.createServer(sslOptions, app).listen(443, () => {
-//   console.log('HTTPS Server running on port 443');
-// });
+https.createServer(sslOptions, app).listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
 
 // Redirect HTTP to HTTPS
 // http.createServer((req, res) => {
