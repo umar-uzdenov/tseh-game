77cb95847c45dea492789469d3321742982bd3c5
umar-uzdenov <90197962+umar-uzdenov@users.noreply.github.com>
2025-04-23 19:50:15 +0300

    Commit all current changes

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.get('/api/test', (req, res) => {
 app.get('/api/user', async (req, res) => {
     try {
         const tgId = Number(req.query.tgid)
+        console.log(req.query)
         res.json(await database.user.get(tgId))
     } catch (error) {
         console.error('Error fetching user:', error);
