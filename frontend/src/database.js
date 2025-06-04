8e83d786e1deb1658ec96712325abe5b7d1989a7
umar-uzdenov <90197962+umar-uzdenov@users.noreply.github.com>
2025-06-04 15:05:47 +0300

    Alpha 0.1

--- frontend/src/database.js
+++ frontend/src/database.js
@@ -35,8 +35,7 @@ window.database = reactive({
         ]
 
         const machines = [
-            1.5, 1.2, 1.8, 1.6, 1.4, 1.55, 1.65, 1.75, 1.85, 1.95,2.05,
-            2.15, 2.25, 2.35, 2.45, 2.55, 2.65, 2.75, 2.85, 2.95
+            1.5, 1.2, 1.8, 1.6, 1.4, 1.5, 2.2
         ].map((price, id) => {
             const machine = brands.random()
             return {
